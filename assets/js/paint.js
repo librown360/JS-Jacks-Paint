de367e8b7f16d3bf2380b5c352677feae865d1a1
Lisa Brown <lisaann493@verizon.net>
2022-01-05 17:16:22 -0500

    initial commit

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,7 +1,7 @@
 function configureListeners() {
-    var images = document.getElementsByTagName('img');    
+    let images = document.getElementsByTagName('img');    
 
-     for (var i = 0; i < images.length; i++) {        
+    for (let i = 0; i < images.length; i++) {        
         document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
         document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
     } 
@@ -14,23 +14,16 @@ function addOpacity(event) {
     getProductInfo(event.target.id);     
 }
 
-function removeOpacity(event) {
+function removeOpacity() {
      if (this.classList.contains('dim')){
         this.classList.remove('dim');
     }
-
+    
     let element = document.getElementById('ppg');
         element.textContent = '';
         
     let color = document.getElementById('color');
-        color.textContent = ''; 
-
-    event.preventDefault();    
-}
-
-function changeImage(elementId) {
-    let image = document.getElementById('imgDisplay');
-    image.src = elementId.src;
+        color.textContent = '';    
 }
 
 function getProductInfo(partNumber) {
@@ -39,60 +32,57 @@ function getProductInfo(partNumber) {
     
     switch (partNumber) {
         case 'pn1':           
-            price = '$19.99'
+            price = '$14.99'
             colorName = 'Lime Green'
             updatePrice(colorName, price)           
             break;           
         case 'pn2':
-            price = '$12.99'
+            price = '$11.14'
             colorName = 'Medium Brown' 
             updatePrice(colorName, price)     
             break;            
         case 'pn3':
-            price = '$11.99'
+            price = '$22.99'
             colorName = 'Royal Blue'   
             updatePrice(colorName, price)   
             break;   
         case 'pn4':
-            price = '$14.99'
-            colorName = 'Bright Red'            
+            price = '$13.42'
+            colorName = 'Solid Red'            
             updatePrice(colorName, price)   
             break;   
         case 'pn5':
-            price = '$9.99'
+            price = '$21.98'
             colorName = 'Solid White' 
             updatePrice(colorName, price)              
             break;   
         case 'pn6':
-            price = '$15.99'
+            price = '$4.99'
             colorName = 'Solid Black'   
             updatePrice(colorName, price)            
             break;   
         case 'pn7':
-            price = '$8.99'
-            colorName = 'Medium Blue'       
+            price = '$8.22'
+            colorName = 'Solid Cyan'       
             updatePrice(colorName, price)        
             break;   
         case 'pn8':
-            price = '$16.99'
-            colorName = 'Light Purple'      
+            price = '$11.99'
+            colorName = 'Solid Purple'      
             updatePrice(colorName, price)         
             break;   
         case 'pn9':
-            price = '$17.99'
-            colorName = 'Bright Yellow'                       
+            price = '$14.99'
+            colorName = 'Solid Yellow'                       
             updatePrice(colorName, price)   
             break;   
           default:              
     }
-
-    function updatePrice(colorName, price)
-    {       
+    function updatePrice(colorName, price) {       
         let ppg = document.getElementById('ppg');
         ppg.textContent = price;
         
         let color = document.getElementById('color');
         color.textContent = colorName;
     }
-    
 }
